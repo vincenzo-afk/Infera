1ba345ae518cddf7893d290c782f2ea995a4ca5a
vincenzo-afk <your@email.com>
2026-07-08 20:25:34 +0530

    Fix unresolved Context reference in MainActivity.kt

--- android/app/src/main/kotlin/com/chaosvoice/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/chaosvoice/app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.chaosvoice.app
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.VpnService
